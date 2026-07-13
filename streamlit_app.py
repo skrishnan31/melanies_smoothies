e278a900328c4c8fee2493b36dcc8784d19243c2
skrishnan31 <75037312+skrishnan31@users.noreply.github.com>
2026-07-12 19:01:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("(ttps://my.smoothiefroot.com/api/fruit/watermelon")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_responsejson())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
